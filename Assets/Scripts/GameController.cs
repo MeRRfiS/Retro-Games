a99d32987b18813960a1916732d34c6bd3ec7570
Egor <uit.merrfis@gmail.com>
2023-09-08 18:12:12 +0300

    Disable Cursor

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,6 +6,8 @@ public class GameController : MonoBehaviour
 {
     private void Awake()
     {
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = false;
         Application.targetFrameRate = 60;
     }
 }
